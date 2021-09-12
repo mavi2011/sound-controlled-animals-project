5d61641155fe8e028ffd639c958a7929443f95e7
mavi2011 <e4.mavi@gmail.com>
2021-09-12 12:36:08 +0530

    save

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sound-controlled-animals-project
- 
+    
--- main.js
+++ main.js
@@ -8,7 +8,27 @@ function modelReady(){
     classifier.classify(gotResults);
 }
 
-function gotResults(error, results){
-    console.log('got result');
+function gotResults(error, results)
+{if(error){
+    console.log(error);
+}else{
+    console.log(results);
+    random_number_r=Math.floor(Math.random()*255)+1;
+    random_number_g=Math.floor(Math.random()*255)+1;
+    random_number_b=Math.floor(Math.random()*255)+1;
+    document.getElementById("result_label").innerHTML='I can Hear - '+results[0].label;
+    document.getElementById("result_confidence").innerHTML='Accuracy - '+(results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+    document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+if (results[0].label == "barking") { 
+    img.src = 'dog barking.gif';
+     }
+     else if (results[0].label == "roaring") { 
+         img1.src = 'roar.gif';
+        } 
+        else if (results[0].label == "meow") {
+             img.src = 'cat'; 
+            }else if (results[0].label == "mooing"){ 
+                img.src = 'cow.gif';
+            } } }
 
-}
